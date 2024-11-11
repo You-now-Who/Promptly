0a8506859529032a06c8949eab229c65e3643305
Yash Yadav <yashyadav.sr@gmail.com>
2024-11-11 15:38:44 +0530

    create posts

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
-  return (
-    <div>Form</div>
+const Form = ({ type, setPost, submitting, handleSubmit }) => {
+
+  return(
+    <>
+      <section className="w-full max-w-full flex-start flex-col">
+        <h1 className="head_text text-left">{type} Post</h1>
+        <span className="blue_gradient">{type} Post</span>
+        
+      </section>
+    </>
   )
-}
+};
 
-export default Form
+export default Form;
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import Form from '@app/components/Form'
 
 const CreatePrompt = () => {
+    const [ submitting, setSubmitting ] = useState(false)
+    const [ post, setPost ] = useState({
+        prompt: '',
+        tag: ''
+    })
+
+    const createPrompt = async (e) => {
+        
+    }
+    
   return (
-    <div>CreatePrompt</div>
+    <>
+        <Form
+        type="Create"
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}/>
+    </>
   )
 }
 
